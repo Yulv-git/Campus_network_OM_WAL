282801f0fedb85262719c436c1599782db04dbfa
Yulv-git <34329208+Yulv-git@users.noreply.github.com>
2021-02-08 12:09:47 +0800

    Update Online_Login_Web.py
    For format.

--- Online_Login_Web.py
+++ Online_Login_Web.py
@@ -14,34 +14,29 @@ import datetime
 def Online_or_not(test_web="https://www.baidu.com"):
     try:
         q=requests.get(test_web, timeout=5)
-        m=re.search(r'STATUS OK', q.text)
-        if m:
-        	print('\n网络连接正常 ~')
-
+        STATUS=re.search(r'STATUS OK', q.text)
+        if STATUS:
+        	print('\n【Check】 网络连接正常~')
         	return True
-
         else:
-        	print('\n网络连接异常 ！')
-
+        	print('\n【Check】 网络连接异常！')
         	return False
-
     except:
-        print('\n断网了 ......')
-
+        print('\n【Check】 断网了 ......')
         return False
 
 
 def Web_Login(name, password):
-  if not Online_or_not(test_web="https://www.baidu.com"):
-    print("{} 网络离线，正在重连...\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+	if not Online_or_not(test_web="https://www.baidu.com"):
 		data = {"DDDDD":name, "upass":password, "R1":0, "R6":0, "para":00, "0MKKey":123456}
 		try:
+			print("【Login】{} 网络离线，正在登录...\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 			result = requests.post(Web_URL, data=data)
-			print("{} 登录成功~\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-    except:
-			print("{} 登录失败，未连接网线或WIFI等！\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+			print("【Login】{} 登录成功~\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+		except:
+			print("【Login】{} 登录失败，设备未连接到网线或WIFI等！\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 	else:
-		print("{} 网络连接正常~\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+		print("【Login】{} 网络连接正常, 无需再次登录~\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 
 
 Web_URL = "https://drcom.szu.edu.cn/a70.htm"
