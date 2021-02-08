a75c0b7be76fff0811d473ccda6ab2dfc2b145c5
Yulv-git <34329208+Yulv-git@users.noreply.github.com>
2021-02-08 22:29:11 +0800

    Update Online_Login_Web.py

--- Online_Login_Web.py
+++ Online_Login_Web.py
@@ -5,6 +5,9 @@ Author: Shuangchi He / Yulv
 Email: yulvchi@qq.com
 Date: 2021-02-08 11:36:34
 Description: 深大校园网  在线监测 & 网页版自动登录
+1、修改用户名和密码，即可运行该脚本；
+2、电脑上设置定时任务（每n秒执行该脚本），即可实现自动检测与登录；
+3、设置后上述两个步骤，即可保证校园网不会掉线超过n秒（当然，需满足：处于开机状态、校园网没欠费、网线或无线连接好、校园网网页端没有变更）。
 '''
 import re
 import requests
@@ -26,17 +29,17 @@ def Online_or_not(test_web="https://www.baidu.com"):
         return False
 
 
-def Web_Login(name, password):
-	if not Online_or_not(test_web="https://www.baidu.com"):
-		data = {"DDDDD":name, "upass":password, "R1":0, "R6":0, "para":00, "0MKKey":123456}
-		try:
-			print("【Login】{} 网络离线，正在登录...\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-			result = requests.post(Web_URL, data=data)
-			print("【Login】{} 登录成功~\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-		except:
-			print("【Login】{} 登录失败，设备未连接到网线或WIFI等！\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-	else:
-		print("【Login】{} 网络连接正常, 无需再次登录~\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+def Web_Login(name=None, password=None):
+    if not Online_or_not(test_web="https://www.baidu.com"):
+        data = {"DDDDD":name, "upass":password, "R1":0, "R6":0, "para":00, "0MKKey":123456}
+            try:
+                print("【Login】{} 网络离线，正在登录...\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+                result = requests.post(Web_URL, data=data)
+		print("【Login】{} 登录成功~\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+        except:
+		print("【Login】{} 登录失败，设备未连接到网线或WIFI等！\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+    else:
+	print("【Login】{} 网络连接正常, 无需再次登录~\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 
 
 Web_URL = "https://drcom.szu.edu.cn/a70.htm"
